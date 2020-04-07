86223883a60fb93c2bb080a712cddf8fc1b52607
Nicolò Battaglia <gaara0633@gmail.com>
2020-04-07 20:21:41 +0200

    Fix

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-$(".fa-microphone").click(function(){
+$(".festivalbar").click(function(){
+  $(".fa-microphone").hide()
+  $(".fa-telegram-plane").show()
+})
+$(".fa-telegram-plane").click(function(){
   var input = $(".festivalbar");
   var inputVal = input.val()
   $("#main").append('<div class="destra row">' + inputVal + '</div>')
